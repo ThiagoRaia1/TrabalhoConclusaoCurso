ad999a991f8984b109da48a69236e49937675e88
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2025-04-30 14:19:54 -0300

    Login funcionando

--- context/api.ts
+++ context/api.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+const API_URL = `http://localhost:3000/usuario`;
+
 export async function autenticarLogin(login: string, senha: string) {
   try {
-    const response = await axios.post(`${process.env.API_URL}/usuario/login`, { login, senha });
+    const response = await axios.post(`${API_URL}/login`, { login, senha });
     console.log(response.data);
     return response.data; // já vem sem senha e _id
   } catch (error) {
     console.warn(error);
-    throw new Error("Erro ao autenticar aluno");
+    throw new Error("Erro ao autenticar usuario");
   }
 }
