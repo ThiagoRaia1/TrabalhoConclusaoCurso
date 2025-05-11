b62652919fbff9b4975a4603ddfe45a7ebb2e9f9
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2025-05-11 15:34:02 -0300

    Backend pelo Render (Precisa substiuir url pra .env)

--- services/urlApi.ts
+++ services/urlApi.ts
@@ -1,2 +1,2 @@
-// export const LOCALHOST_URL = "https://backend-tg-2dnx.onrender.com"
-export const LOCALHOST_URL = "http://localhost:3000"
+export const LOCALHOST_URL = "https://backend-tg-2dnx.onrender.com" // Render
+// export const LOCALHOST_URL = "http://localhost:3000"
