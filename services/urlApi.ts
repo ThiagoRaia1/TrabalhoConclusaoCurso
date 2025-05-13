af71ca8081f50e3e7aae750b5d38d113a114f594
Thiago Raia <143272411+ThiagoRaia1@users.noreply.github.com>
2025-05-13 23:20:46 +0000

    Backend no Render

--- services/urlApi.ts
+++ services/urlApi.ts
@@ -1,2 +1,2 @@
-// export const LOCALHOST_URL = EXPO_PUBLIC_RENDER_BACKEND_API_URL // Render
-export const LOCALHOST_URL = process.env.EXPO_PUBLIC_LOCALHOST_URL
+export const LOCALHOST_URL = process.env.EXPO_PUBLIC_RENDER_BACKEND_API_URL // Render
+// export const LOCALHOST_URL = process.env.EXPO_PUBLIC_LOCALHOST_URL
